a749d46f57220eb2a8e481a8d703c9ec71dcf0b9
Vidisha23saini <vidisha23saini@gmail.com>
2025-09-05 18:26:34 +0100

    Prepare Django for Render deployment

--- ocean_rest_main/settings.py
+++ ocean_rest_main/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,3 +124,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_URL = '/static/'
+
+# Collect static files here when you deploy
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Enable WhiteNoise to compress and cache static files
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
